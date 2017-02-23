f98389ea2c4705979a6075e4e387d106fd4af655
Timothy Dry <timothy.dry@verizon.net>
2017-02-23 12:28:02 -0500

    added a clear all buytton

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,17 +10,13 @@ var movieArray = ["Titanic", "Beverly Hills Cop", "The Princess Bride", "The Lor
 
 function displayGiff() {
 
-       console.log(this);
        var title = $(this).attr('data-name');
        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + title + "&api_key=dc6zaTOxFJmzC&limit=12";
 $.ajax({
       url: queryURL,
       method: 'GET'
     }).done(function(response) {
-    	//document.write(response.data[1]);
-      console.log(response.data[0]);
-      //console.log(response.data[0].user.display_name);
-      var j = 0;
+    
       for (i=0; i < 12; i++) {
       	//Generate the images with the links for moving and still images embeded
         $("#display"+ i).html("<img src='" + response.data[i].images.fixed_height_still.url + "'" +
@@ -29,7 +25,9 @@ $.ajax({
         						"data='still' class='image'>");
         // put ratings in upper case letters
         var y = response.data[i].rating.toUpperCase();
+        // add rating above image
         $("#rating"+ i).html("<h3>Rating: " + y + "</h3>");
+        // add original url and link to open in another window below image
         $("#title"+ i).html("<h5><a href='"+response.data[i].images.original.url+"' target='_blank'>"+ response.data[i].images.original.url + "</h5>");
       }
     });
@@ -82,25 +80,29 @@ $(document).ready(function () {
 
     	// This line grabs the input from the textbox
     	var movie = $("#movie-input").val().trim();
-    	console.log(movie);;
-    	if (movie === "") {    	}
-    	else {
 
-    	// The movie from the textbox is then added to our array
-    	movieArray.push(movie);
-
-    	// Calling renderButtons which handles the processing of our movie array
-		renderButtons();
-		$('#movie-input').val("");
-    	}
-
+    	// if nothing is inputed do nothing
+    	if (!(movie === "")) {   
+    		// The movie from the textbox is then added to our array
+    		movieArray.push(movie);
+    		// Calling renderButtons which handles the processing of our movie array
+			renderButtons();
+			$('#movie-input').val(""); // clear input field
+    		}
      });
+
 	// on click events to display the giff and
 	// switch the still giff for the moving giff and back
 	$(document).on("click", ".movie", displayGiff);
 	$(document).on("click", ".image", switchImage);
 	$("#reset").on("click", function () {
+		event.preventDefault();
 		movieArray = ["Titanic", "Beverly Hills Cop", "The Princess Bride", "The Lord of the Rings", "Monty Python and the Holy Grail", "Star Wars", "Do the Right Thing"];
+		renderButtons();
+	});
+	$("#resetall").on("click", function () {
+		event.preventDefault();
+		movieArray = [];
 		renderButtons();
 	});
 });
